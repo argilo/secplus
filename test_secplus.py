540e6472fbd487a4c1ba012079ab61a9e8b235c6
Clayton Smith <argilo@gmail.com>
2020-08-18 19:28:41 -0400

    That that limits are enforced

--- test_secplus.py
+++ test_secplus.py
@@ -17,6 +17,20 @@ class TestSecplus(unittest.TestCase):
             self.assertEqual(rolling, rolling_out)
             self.assertEqual(fixed, fixed_out)
 
+    def test_encode_rolling_limit(self):
+        rolling = 2**32
+        fixed = 3**20 - 1
+
+        with self.assertRaisesRegex(ValueError, "Rolling code must be less than 2\^32"):
+            secplus.encode(rolling, fixed)
+
+    def test_encode_fixed_limit(self):
+        rolling = 2**32 - 1
+        fixed = 3**20
+
+        with self.assertRaisesRegex(ValueError, "Fixed code must be less than 3\^20"):
+            secplus.encode(rolling, fixed)
+
     def test_encode_v2_decode_v2(self):
         for _ in range(100000):
             rolling = random.randrange(2**28)
@@ -26,6 +40,20 @@ class TestSecplus(unittest.TestCase):
 
             self.assertEqual(rolling, rolling_out)
             self.assertEqual(fixed, fixed_out)
+
+    def test_encode_v2_rolling_limit(self):
+        rolling = 2**28
+        fixed = 2**40 - 1
+
+        with self.assertRaisesRegex(ValueError, "Rolling code must be less than 2\^28"):
+            secplus.encode_v2(rolling, fixed)
+
+    def test_encode_v2_fixed_limit(self):
+        rolling = 2**28 - 1
+        fixed = 2**40
+
+        with self.assertRaisesRegex(ValueError, "Fixed code must be less than 2\^40"):
+            secplus.encode_v2(rolling, fixed)
 
 
 if __name__ == '__main__':
