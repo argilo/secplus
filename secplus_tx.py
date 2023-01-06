1b56a821b7b225c5d073171fb55b69ae5493e1e9
Clayton Smith <argilo@gmail.com>
2023-01-06 14:48:22 -0500

    Regenerate flow graphs

--- secplus_tx.py
+++ secplus_tx.py
@@ -6,7 +6,7 @@
 #
 # GNU Radio Python Flow Graph
 # Title: Secplus Tx
-# GNU Radio version: 3.9.3.0
+# GNU Radio version: 3.10.5.0
 
 from gnuradio import analog
 from gnuradio import blocks
@@ -53,7 +53,7 @@ class secplus_tx(gr.top_block):
         )
         self.osmosdr_sink_0.set_time_unknown_pps(osmosdr.time_spec_t())
         self.osmosdr_sink_0.set_sample_rate(samp_rate)
-        self.osmosdr_sink_0.set_center_freq(freq - 300e3, 0)
+        self.osmosdr_sink_0.set_center_freq((freq - 300e3), 0)
         self.osmosdr_sink_0.set_freq_corr(0, 0)
         self.osmosdr_sink_0.set_gain(0, 0)
         self.osmosdr_sink_0.set_if_gain(47, 0)
@@ -64,7 +64,6 @@ class secplus_tx(gr.top_block):
         self.blocks_repeat_0 = blocks.repeat(gr.sizeof_gr_complex*1, 1000)
         self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
         self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 300e3, 0.9, 0, 0)
-
 
 
         ##################################################
@@ -89,7 +88,7 @@ class secplus_tx(gr.top_block):
 
     def set_freq(self, freq):
         self.freq = freq
-        self.osmosdr_sink_0.set_center_freq(self.freq - 300e3, 0)
+        self.osmosdr_sink_0.set_center_freq((self.freq - 300e3), 0)
 
     def get_rolling(self):
         return self.rolling
