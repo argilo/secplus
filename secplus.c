9bd6f7d6c4004fd3e2e47bc6d64d11411def9de4
Clayton Smith <argilo@gmail.com>
2023-01-06 21:19:02 -0500

    Get rid of rolling arrays

--- secplus.c
+++ secplus.c
@@ -17,54 +17,54 @@ static const uint8_t _INVERT[11][3] = {
     {1, 1, 0}, {0, 1, 0}, {0, 0, 1}, {0, 0, 0}, {1, 1, 1}, {1, 0, 1},
     {0, 1, 1}, {0, 0, 0}, {1, 0, 0}, {0, 0, 0}, {1, 0, 1}};
 
-static void _encode_v2_rolling(uint32_t rolling, uint8_t *rolling1,
-                               uint8_t *rolling2) {
+static void _encode_v2_rolling(uint32_t rolling, uint32_t *rolling1,
+                               uint32_t *rolling2) {
   uint32_t rolling_reversed = 0;
 
   for (int bit = 0; bit < 28; bit++) {
     rolling_reversed |= ((rolling >> bit) & 1) << (28 - bit - 1);
   }
 
-  rolling1[3] = rolling_reversed % 3;
+  *rolling1 = rolling_reversed % 3;
   rolling_reversed /= 3;
-  rolling1[2] = rolling_reversed % 3;
+  *rolling1 |= (rolling_reversed % 3) << 2;
   rolling_reversed /= 3;
-  rolling1[1] = rolling_reversed % 3;
+  *rolling1 |= (rolling_reversed % 3) << 4;
   rolling_reversed /= 3;
-  rolling1[0] = rolling_reversed % 3;
-  rolling_reversed /= 3;
-
-  rolling2[3] = rolling_reversed % 3;
-  rolling_reversed /= 3;
-  rolling2[2] = rolling_reversed % 3;
-  rolling_reversed /= 3;
-  rolling2[1] = rolling_reversed % 3;
-  rolling_reversed /= 3;
-  rolling2[0] = rolling_reversed % 3;
+  *rolling1 |= (rolling_reversed % 3) << 6;
   rolling_reversed /= 3;
 
-  rolling1[7] = rolling_reversed % 3;
+  *rolling2 = rolling_reversed % 3;
   rolling_reversed /= 3;
-  rolling1[6] = rolling_reversed % 3;
+  *rolling2 |= (rolling_reversed % 3) << 2;
   rolling_reversed /= 3;
-  rolling1[5] = rolling_reversed % 3;
+  *rolling2 |= (rolling_reversed % 3) << 4;
   rolling_reversed /= 3;
-  rolling1[4] = rolling_reversed % 3;
-  rolling_reversed /= 3;
-
-  rolling2[7] = rolling_reversed % 3;
-  rolling_reversed /= 3;
-  rolling2[6] = rolling_reversed % 3;
-  rolling_reversed /= 3;
-  rolling2[5] = rolling_reversed % 3;
-  rolling_reversed /= 3;
-  rolling2[4] = rolling_reversed % 3;
+  *rolling2 |= (rolling_reversed % 3) << 6;
   rolling_reversed /= 3;
 
-  rolling1[8] = rolling_reversed % 3;
+  *rolling1 |= (rolling_reversed % 3) << 10;
+  rolling_reversed /= 3;
+  *rolling1 |= (rolling_reversed % 3) << 12;
+  rolling_reversed /= 3;
+  *rolling1 |= (rolling_reversed % 3) << 14;
+  rolling_reversed /= 3;
+  *rolling1 |= (rolling_reversed % 3) << 16;
   rolling_reversed /= 3;
 
-  rolling2[8] = rolling_reversed % 3;
+  *rolling2 |= (rolling_reversed % 3) << 10;
+  rolling_reversed /= 3;
+  *rolling2 |= (rolling_reversed % 3) << 12;
+  rolling_reversed /= 3;
+  *rolling2 |= (rolling_reversed % 3) << 14;
+  rolling_reversed /= 3;
+  *rolling2 |= (rolling_reversed % 3) << 16;
+  rolling_reversed /= 3;
+
+  *rolling1 |= (rolling_reversed % 3) << 8;
+  rolling_reversed /= 3;
+
+  *rolling2 |= (rolling_reversed % 3) << 8;
 }
 
 static void _v2_calc_parity(uint64_t fixed, uint32_t *data) {
@@ -101,30 +101,21 @@ static void _v2_scramble(uint32_t *parts, int type, uint8_t *packet_half) {
   }
 }
 
-static void _encode_v2_half_parts(uint8_t *rolling, uint32_t fixed,
+static void _encode_v2_half_parts(uint32_t rolling, uint32_t fixed,
                                   uint16_t data, int type,
                                   uint8_t *packet_half) {
   uint32_t parts[3] = {0};
 
   parts[0] = ((fixed >> 10) << 8) | (data >> 8);
   parts[1] = ((fixed & 0x3ff) << 8) | (data & 0xff);
+  parts[2] = rolling;
 
-  parts[2] |= (rolling[4] << 16);
-  parts[2] |= (rolling[5] << 14);
-  parts[2] |= (rolling[6] << 12);
-  parts[2] |= (rolling[7] << 10);
-  parts[2] |= (rolling[8] << 8);
-  parts[2] |= (rolling[0] << 6);
-  parts[2] |= (rolling[1] << 4);
-  parts[2] |= (rolling[2] << 2);
-  parts[2] |= rolling[3];
-
-  packet_half[0] = (uint8_t)parts[2];
+  packet_half[0] = (uint8_t)rolling;
 
   _v2_scramble(parts, type, packet_half);
 }
 
-static void _encode_v2_half(uint8_t *rolling, uint32_t fixed, uint16_t data,
+static void _encode_v2_half(uint32_t rolling, uint32_t fixed, uint16_t data,
                             int type, uint8_t *packet_half) {
   _encode_v2_half_parts(rolling, fixed, data, type, packet_half);
 
@@ -136,17 +127,17 @@ static void _encode_v2_half(uint8_t *rolling, uint32_t fixed, uint16_t data,
   packet_half[0] |= (type << 6);
 }
 
-static void _encode_wireline_half(uint8_t *rolling, uint32_t fixed,
+static void _encode_wireline_half(uint32_t rolling, uint32_t fixed,
                                   uint16_t data, uint8_t *packet_half) {
   _encode_v2_half_parts(rolling, fixed, data, 1, packet_half);
 }
 
 int encode_v2(uint32_t rolling, uint64_t fixed, uint32_t data, int type,
               uint8_t *packet) {
-  uint8_t rolling1[9], rolling2[9];
+  uint32_t rolling1, rolling2;
   int packet_len = (type == 0 ? 10 : 16);
 
-  _encode_v2_rolling(rolling, rolling1, rolling2);
+  _encode_v2_rolling(rolling, &rolling1, &rolling2);
   _v2_calc_parity(fixed, &data);
 
   for (int i = 0; i < packet_len; i++) {
@@ -162,9 +153,9 @@ int encode_v2(uint32_t rolling, uint64_t fixed, uint32_t data, int type,
 
 int encode_wireline(uint32_t rolling, uint64_t fixed, uint32_t data,
                     uint8_t *packet) {
-  uint8_t rolling1[9], rolling2[9];
+  uint32_t rolling1, rolling2;
 
-  _encode_v2_rolling(rolling, rolling1, rolling2);
+  _encode_v2_rolling(rolling, &rolling1, &rolling2);
   _v2_calc_parity(fixed, &data);
 
   packet[0] = 0x55;
